987138a86e8ac7f4b9a5f104aa8a5b7e121a9d40
OksanaSemenovska <oksanakononchuk1@ukr.net>
2022-12-01 22:37:24 +0200

    task2

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -23,15 +23,15 @@ console.log(message);
 
 
 
-  const categoriesItem = document.querySelectorAll(".item");
-  categoriesItem.forEach((item) => {
-      console.log(`Category: ${item.firstElementChild.textContent}`);
-      console.log(`Elements: ${item.lastElementChild.children.length}`);
-  });
+   const categoriesItem = document.querySelectorAll(".item");
+   categoriesItem.forEach((item) => {
+       console.log(`Category: ${item.firstElementChild.textContent}`);
+       console.log(`Elements: ${item.lastElementChild.children.length}`);
+   });
 
 // 1. Нам потрібно зайти в клас item.Робимо за допомогою querySelectorAll;
 // 2. Далі потрібно перебрати масив, де 1. буде категорія  (це h2),
-//     2 - елемент(кількість по списку li); (перебираємо за методом forEach, він буде 
+//     2 - елемент(кількість по списку li); (перебираємо за методом forEach, він буде
 //         перебирати всі елементи до кінця);
 // 3. Створюємо функцію, де відразу виводимо в консоль:
 // - (`Category: ${item.firstElementChild.textContent}`)
@@ -42,7 +42,9 @@ console.log(message);
 // тільки довжину цих лішок всіх.тому рахуємо всіх дітей children + length, так як нам треба саме кількість(довжина)
 
 
-// і того за допомогою forEach воно пройдеться по всьому списку який нам треба. і в консоль виведе всі елементи
+// і того за допомогою forEach воно пройдеться по всьому списку який нам треба. і в консоль виведе всі елементи;
+
+
 
 
 
